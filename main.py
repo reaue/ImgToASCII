9b62eccdca4b7a3a7468403c6bcfd317900cb195
reaue <mathias.crama.second@gmail.com>
2026-09-02 12:32:30 +0200

    test

---
+++ main.py
@@ -0,0 +1,3 @@
+# This program must transform images to ASCII with brightness and colour.
+# The brightness can be represented by the following characters ".:-=+*#%@" in ASCII
+# The finale goal is to add also transformation for videos becaus it's just a sequence of images. 
